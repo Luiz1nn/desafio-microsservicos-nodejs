64eb0819c237f0f02c66d24fcf8be30315f7ac51
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-05 19:53:14 -0300

    🔧: ci: add biome linter and format existing code

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,12 +1,18 @@
-import {fastify} from "fastify";
-import {serializerCompiler, validatorCompiler, type ZodTypeProvider} from "fastify-type-provider-zod";
-import {fastifyCors} from "@fastify/cors";
+import { fastify } from 'fastify'
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.register(fastifyCors, { origin: '*'})
+app.register(fastifyCors, { origin: '*' })
 
-app.listen({ host: '0.0.0.0', port: 3333}).then(() => console.log('[Orders] HTTP Server is running!'))
+app
+	.listen({ host: '0.0.0.0', port: 3333 })
+	.then(() => console.log('[Orders] HTTP Server is running!'))
