cfa1eea4eb025b04fdf1d542a6ee66ece301c5da
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-07 01:44:54 -0300

    ✨ feat: add initial database schema, migrations, and configuration for

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -5,6 +5,10 @@ import {
 	type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
+import {z} from "zod";
+import { randomUUID } from 'node:crypto'
+import {db} from "../db/client.ts";
+import {schema} from "../db/schema/index.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,6 +19,28 @@ app.register(fastifyCors, { origin: '*' })
 
 app.get('/health', () => {
   return 'OK'
+})
+
+app.post('/orders', {
+  schema: {
+    body: z.object({
+      amount: z.number(),
+    })
+  }
+}, async (request, reply) => {
+  const { amount } = request.body
+
+  console.log('Creating an order with amount', amount)
+
+  const orderId = randomUUID()
+
+  await db.insert(schema.orders).values({
+    id: orderId,
+    customerId: '0b41961f-c250-47ed-abef-2a81bfdd7708',
+    amount: 30,
+  });
+
+  return reply.status(201).send()
 })
 
 app
